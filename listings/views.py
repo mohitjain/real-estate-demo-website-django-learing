57627c063b91935d77057ad3811a7fd4f30c5f5a
Mohit Jain <jain.mohit27@gmail.com>
2020-08-07 03:48:38 +0530

    Listing Show page

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 
 def index(request):
@@ -16,7 +16,11 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
