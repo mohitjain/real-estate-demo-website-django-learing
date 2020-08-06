0e4f0680aae2eb3d9fb9178b0e212ef2253d5477
Mohit Jain <jain.mohit27@gmail.com>
2020-08-07 04:47:38 +0530

    Links for Login/Register and Dashboard with dummy template

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def logout(request):
+    return redirect('index')
--- btre/settings.py
+++ btre/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'cejabwqf_q_!uxi%d7of9u*)uhax6r!r5qox#1g$j@g^0%s83('
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
+    'accounts.apps.AccountsConfig',
     'realtors.apps.RealtorsConfig',
     'django.contrib.humanize',
 ]
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'btre.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -88,7 +86,6 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -108,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -121,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
