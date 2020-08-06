0bbeb0b35bec9b5e2d9c59ef7e9ec8e3df428c41
Mohit Jain <jain.mohit27@gmail.com>
2020-08-07 05:09:18 +0530

    Messaging setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        messages.error(request, "This is a sample error")
+        return redirect('register')
+    else:
+        return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == "POST":
+        print("Register")
+    else:
+        return render(request, 'accounts/register.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
