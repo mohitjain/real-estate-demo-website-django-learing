a7e4ab5aa159cb5b63290671739946ec9ba33da2
Mohit Jain <jain.mohit27@gmail.com>
2020-08-07 22:44:42 +0530

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,16 @@ from django.contrib.auth.models import User
 
 def login(request):
     if request.method == "POST":
-        messages.error(request, "This is a sample error")
-        return redirect('register')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Credentials Invalid")
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
